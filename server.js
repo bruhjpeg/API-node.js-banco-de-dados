88c8d55c3560e3d96bc37845a2bd0e94321388b3
Bruna Assis <105996694+bruhjpeg@users.noreply.github.com>
2025-06-05 15:53:11 -0300

    entendendo http status

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
-app.get("/usuarios", (request, response) => {});
+const users = [];
+
+app.post("/usuarios", (request, response) => {
+  users.push(request.body);
+
+  response.send("Usuario criado com sucesso!!");
+});
+
+app.get("/usuarios", (request, response) => {
+  response.json(users);
+});
+
+app.listen(3000);
+
+// Criar nossa API de usuarios
+
+// Criar um usuario
+// listar todos os usuarios
+// Editar um usuario
+// Deletar um usuario
